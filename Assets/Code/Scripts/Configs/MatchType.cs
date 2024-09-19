206f503c5fb20c7d10ca88169bccf650cd9b97e1
Gaba <gabrielfpaula@gmail.com>
2024-09-19 15:10:28 -0300

    Add MatchType
    Match Types are a configuration that tells the game what a match may be

---
+++ Assets/Code/Scripts/Configs/MatchType.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Code.Scripts.Configs
+{
+    [CreateAssetMenu(fileName = "New MatchType", menuName = "GameConfigs/MatchType")]
+    public class MatchType : ScriptableObject
+    {
+            [Multiline] public string matchFormat;
+
+            public int[,] GetMatchFormat()
+            {
+                var lines = matchFormat.Split('\n');
+                var height = lines.Length;
+                var width = lines[0].Length;
+                var result = new int[height, width];
+                for (var i = 0; i < height; i++)
+                {
+                    var line = lines[i];
+                    var values = line.Split(',');
+                    for (int j = 0; j < width; j++)
+                    {
+                        result[i, j] = int.Parse(values[j]);
+                    }
+                }
+
+                return result;
+            }
+    }
+}
