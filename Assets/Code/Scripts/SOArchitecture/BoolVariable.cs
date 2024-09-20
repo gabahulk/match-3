477690e9cf62948b143caa7c472239b53750eafe
Gaba <gabrielfpaula@gmail.com>
2024-09-20 12:39:10 -0300

    Add board lock variable

---
+++ Assets/Code/Scripts/SOArchitecture/BoolVariable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Code.Scripts.SOArchitecture
+{
+    [CreateAssetMenu(menuName = "Code/Variables/Bool Variable")]
+    public class BoolVariable: ScriptableObject
+    {
+        
+#if UNITY_EDITOR
+            [Multiline]
+            public string developerDescription = "";
+#endif
+            public bool value;
+
+            public void SetValue(bool newValue)
+            {
+                value = newValue;
+            }
+
+            public void SetValue(GameObjectVariable newValue)
+            {
+                value = newValue.value;
+            }
+        }
+}
